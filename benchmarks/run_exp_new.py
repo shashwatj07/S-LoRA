0c467620445f57b491f2bd17c695c7ad62d2f3b2
Shrikara Arun <t-sarun@microsoft.com>
2025-08-14 11:20:05 +0000

    contiguous baseline, more logging

--- benchmarks/run_exp_new.py
+++ benchmarks/run_exp_new.py
@@ -61,7 +61,7 @@ async def send_request(
     else:
         url = server + "/generate_stream"
     
-    if backend in ["dm", "system", "baseline"]:
+    if backend in ["dm", "system", "baseline", "contiguous"]:
         data = {
             'model_dir': model_dir,
             'lora_dir': adapter_dir,
@@ -116,13 +116,14 @@ async def send_request(
 
     request_end_time = time.time()
     request_latency = request_end_time - request_start_time
+    tbt = (request_latency - first_token_latency) / (output_len - 1) if output_len > 1 else 0
     log_line = f"req_id {req_id} {server} adapter_dir {adapter_dir} prompt_len {prompt_len} output_len {output_len} "\
-          f"request_latency {request_latency:.2f} s, first_token_latency {first_token_latency:.2f} s\n"
+          f"request_latency {request_latency:.2f} s, first_token_latency {first_token_latency:.2f} s, tbt {tbt:.2f} s\n"
     print(log_line)
     with open(f"fine_{output_file}", "a") as f:
         f.write(log_line)
-    REQUEST_LATENCY.append((prompt_len, output_len, request_latency, first_token_latency))
-    return (prompt_len, output_len, request_latency, first_token_latency)
+    REQUEST_LATENCY.append((prompt_len, output_len, request_latency, first_token_latency, tbt))
+    return (prompt_len, output_len, request_latency, first_token_latency, tbt)
 
 async def benchmark_baseline(
     backend: str,
@@ -172,12 +173,17 @@ async def benchmark_system(
             adapter_demand = []
             for adapter, tps in demand_tps.items():
                 rank = int(re.search(r'rank-(\d+)', adapter).group(1))
-                adapter_demand.append((rank, tps, adapter))
+                adapter_demand.append((rank, tps, adapter)) # tps here is expected tps
             adapter_demand.sort(reverse=True)
 
-            server_tps = {8: 2400, 16: 2100, 32: 1900, 64:1700, 128:1600}
+            server_tps = {8: 2400, 16: 2100, 32: 1900, 64:1700, 128:1600} # operating point, fn of max rank
+            # TODO score = expected total tps / max rank operating point
+            # assume 2000 as operating point of adapter for score of adapters
+
+            #s1: 128 64 32
+            #s2: 16 8
+
             # greedy bin packing
-            
             def is_compatible(group, tuple, scale=1):
                 ranks = [r for  r, _, _ in group]
                 ranks.append(tuple[0])
@@ -209,10 +215,20 @@ async def benchmark_system(
                     x += 1
                     adapter_groups[x].append(adapter_tuple)
                 else:
+                    # TODO log to new log file that overallocation happened for these adapters
+                    with open("allocation_log.txt", "a") as f:
+                        f.write(f"{last_time} Overallocation for adapters from index {i} to {len(adapter_demand) - 1} ({(len(adapter_demand) - 1 - i + 1) / len(adapter_demand) * 100:.2f} % of total adapters)\n")
                     for j in range(i, len(adapter_demand)):
                         adapter_groups[j % len(adapter_groups)].append(adapter_demand[j])
-                        break
-            print(adapter_groups)
+                    break
+
+            print(adapter_groups) # TODO  move to logfile with timestamp last_time
+            # TODO: log tps with s1:[(adapter, tps)], s2:[(adapter, tps)]
+            with open("allocation_log.txt", "a") as f:
+                f.write(f"\n{last_time} Adapter groups:\n")
+                for i, group in enumerate(adapter_groups):
+                    f.write(f"  Server {servers[i]}: {[(adapter, tps) for _, tps, adapter in group]}\n")
+                    
             server_map = {}
             for i, server in enumerate(servers):
                 for _, _, adapter in adapter_groups[i]:
@@ -256,33 +272,46 @@ def get_res_stats(per_req_latency, benchmark_time, backend, warmup_time=0, warmu
     #     peak_mem = get_peak_mem(server)
     #     print(f"GPU peak memory (GB):", [[f"{x / GB:.2f}" for x in tpg] for tpg in peak_mem])
     print(f"Total time: {benchmark_time:.2f} s")
+    print(f"Non warmup time: {benchmark_time - warmup_time:.2f} s")
+    print(f"Total requests: {len(per_req_latency)}")
+    print(f"Non warmup requests: {len(per_req_latency) - warmup_num}")
     print(f"Aborted Request: {num_abort}")
     print(f"Throughput: {throughput:.2f} requests/s")
 
     strip_throughput = (len(per_req_latency) - warmup_num * 2) / (benchmark_time - warmup_time * 2)
     print(f"Throughput strip: {strip_throughput:.2f} requests/s")
 
-    percentiles = [10, 25, 50, 75, 90,95, 99]
-    e2e_latencies = [latency for _, _, latency, _ in per_req_latency]
+    # Exclude first warmup_num requests from further statistics
+    percentiles = [10, 25, 50, 75, 90, 95, 99]
+    if warmup_num >= len(per_req_latency):
+        raise ValueError("Warmup number exceeds number of requests whose latency was logged")
+    else:
+        stats_base = per_req_latency[warmup_num:]
+
+    e2e_latencies = [latency for _, _, latency, _, _ in stats_base]
     per_token_latencies = [
         latency / (prompt_len + output_len)
-        for prompt_len, output_len, latency, _ in per_req_latency
+        for prompt_len, output_len, latency, _, _ in stats_base
+        if (prompt_len + output_len) > 0
     ]
     per_output_token_latencies = [
         latency / output_len
-        for _, output_len, latency, _ in per_req_latency
+        for _, output_len, latency, _, _ in stats_base
+        if output_len > 0
     ]
-    first_token_latencies = [latency for _, _, _, latency in per_req_latency]
-    abort_satisfaction = [0]*num_abort
-    satisfactions = [reward(latency) for _, _, _, latency in per_req_latency] + abort_satisfaction
-    attainments = [attainment_func(latency) for _, _, _, latency in per_req_latency] + abort_satisfaction
+    first_token_latencies = [latency for _, _, _, latency, _ in stats_base]
+    tbts = [latency for _, _, _, _, latency in stats_base]
+    num_abort = len([i for i in stats_base if i[3] is None])
+    abort_satisfaction = [0] * num_abort
+    satisfactions = [reward(latency) for _, _, _, latency, _ in stats_base] + abort_satisfaction
+    attainments = [attainment_func(latency) for _, _, _, latency, _ in stats_base] + abort_satisfaction
 
-    # Build statistics in a loop to avoid repetition
     metrics = {
         "e2e": e2e_latencies,
         "per_token": per_token_latencies,
         "per_output_token": per_output_token_latencies,
         "first_token": first_token_latencies,
+        "tbt": tbts,
         "satisfaction": satisfactions,
         "attainment": attainments,
     }
@@ -324,7 +353,7 @@ def read_requests(trace_file):
     requests.sort(key=lambda r: r.req_time)
     return list(adapter_dirs), requests
 
-def run_exp(backend, servers, trace_file, output, debug=False):
+def run_exp(backend, servers, trace_file, output, debug=False, warmup_time:int = 60, warmup_num: int = 600):
     # first generate your data using real_trace/clean_chat_data.py
     # base_model = BASE_MODEL[model_setting]
     # adapter_dirs = LORA_DIR[model_setting]
@@ -359,9 +388,14 @@ def run_exp(backend, servers, trace_file, output, debug=False):
         per_req_latency = asyncio.run(benchmark_baseline(backend, server_map, requests, output, debug))
         benchmark_end_time = time.time()
         benchmark_time = benchmark_end_time - benchmark_start_time
-    elif backend == "system":
+    elif backend == "system" or backend == "contiguous":
         # benchmark
-        adapters = sorted(adapter_dirs)
+        adapters = []
+        for adapter in adapter_dirs:
+            rank = int(re.search(r'rank-(\d+)', adapter).group(1))
+            adapters.append((rank, adapter))
+        adapters.sort()
+        adapters = [x[1] for x in adapters[:]]
         
         # Split shuffled list into n parts as evenly as possible
         k, m = divmod(len(adapters), len(servers))
@@ -371,14 +405,19 @@ def run_exp(backend, servers, trace_file, output, debug=False):
             end = (i + 1) * k + min(i + 1, m)
             for adapter in adapters[start:end]:
                 server_map[adapter] = server_name
-        print(server_map)
-        benchmark_start_time = time.time()
-        per_req_latency = asyncio.run(benchmark_system(backend, server_map, adapter_dirs, requests, output, debug))
-        benchmark_end_time = time.time()
+        print("server map ", server_map)
+        benchmark_start_time, benchmark_end_time = 0, 0
+
+        if backend == "system":
+            benchmark_start_time = time.time()
+            per_req_latency = asyncio.run(benchmark_system(backend, server_map, adapter_dirs, requests, output, debug))
+            benchmark_end_time = time.time()
+        elif backend == "contiguous":
+            benchmark_start_time = time.time()
+            per_req_latency = asyncio.run(benchmark_baseline(backend, server_map, requests, output, debug))
+            benchmark_end_time = time.time()
         benchmark_time = benchmark_end_time - benchmark_start_time
 
-    warmup_time = 10
-    warmup_num = int(32)
     res = get_res_stats(per_req_latency, benchmark_time, backend,
                         warmup_time=warmup_time, warmup_num=warmup_num)
 
@@ -389,7 +428,7 @@ def run_exp(backend, servers, trace_file, output, debug=False):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--backend", type=str, required=True,
-                        choices=["system", "baseline"])
+                        choices=["system", "baseline", "contiguous"])
 
     # parser.add_argument("--model-setting", type=str, default="S1")
     parser.add_argument("--debug", action="store_true")
@@ -405,6 +444,10 @@ if __name__ == "__main__":
     parser.add_argument("--servers", "-s", type=str, nargs="+", required=True)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--output", type=str, default=None)
+    parser.add_argument("--warmup-time", type=int, default=60, 
+                        help="Number of seconds considered as warmup, excluded from stats")
+    parser.add_argument("--warmup-requests", type=int, default=600,
+                        help="Number of requests considered as warmup, excluded from stats (rps * warmup-time)")
     args = parser.parse_args()
 
     assert not args.no_lora_copy or args.no_lora_compute
@@ -428,6 +471,7 @@ if __name__ == "__main__":
 
     if not args.append:
         os.system(f"rm {args.output}")
+        os.system(f"rm allocation_log.txt")
         results = []
     else:
         with open(args.output, "r") as f:
@@ -437,4 +481,4 @@ if __name__ == "__main__":
     # for config in tqdm(suites, desc="suites"):
     #     if to_dict(config) not in results:
     stats = run_exp(args.backend, args.servers, args.trace_file_path,
-                            args.output, args.debug)
+                            args.output, args.debug, args.warmup_time, args.warmup_requests)
