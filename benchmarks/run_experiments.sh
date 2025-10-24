445e3755d9159ca4c8f713ae2a44a400ec8c78fe
Shrikara Arun <t-sarun@microsoft.com>
2025-10-24 06:52:26 +0000

    better output organization

--- benchmarks/run_experiments.sh
+++ benchmarks/run_experiments.sh
@@ -34,10 +34,12 @@ for qps in "${qps_list[@]}"; do
                         echo "[$(date -u +'%Y-%m-%dT%H:%M:%SZ')] FAILED qps=${qps} distribution=${distribution} arrival=${arrival} backend=${backend} exit_code=${rc}" >> ../../outputs/run_log.txt
                     fi
                 fi
+                mv allocation_log.txt ${distribution}_${arrival}_${qps}.0_${duration}_${backend}_allocation_log.txt
 
-                cp ${distribution}_${arrival}_${qps}.0_${duration}_${backend}.txt ../../outputs
-                cp fine_${distribution}_${arrival}_${qps}.0_${duration}_${backend}.txt ../../outputs
-
+                mkdir -p ../../outputs/${distribution}_${arrival}_${qps}/${backend}
+                cp ${distribution}_${arrival}_${qps}.0_${duration}_${backend}.txt ../../outputs/${distribution}_${arrival}_${qps}/${backend}
+                cp fine_${distribution}_${arrival}_${qps}.0_${duration}_${backend}.txt ../../outputs/${distribution}_${arrival}_${qps}/${backend}
+                cp ${distribution}_${arrival}_${qps}.0_${duration}_${backend}_allocation_log.txt ../../outputs/${distribution}_${arrival}_${qps}/${backend}
             done
         done
     done
