0a0e78295c06fd022a0b1d00050123de811099d2
Shrikara Arun <t-sarun@microsoft.com>
2025-11-10 11:18:12 +0000

    fig 5h 4 nodes scalability data

---
+++ benchmarks/5h/rescale_num_adapters.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import json
+import re
+
+
+rps_list = [34]
+num_adapters_per_rank = 10 # total 50 adapters
+
+for rps in rps_list:
+    counters = {8: 0, 16: 0, 32: 0, 64: 0, 128: 0}
+    df = pd.read_csv(f"../experiment_traces_v2/uniform_poisson_{rps}.0_900.csv")
+    for idx, row in df.iterrows():
+        rank = int(re.search(r"rank-(\d+)", row["adapter_dir"]).group(1))
+        adapter = f"dummy-lora-7b-rank-{rank}-{counters[rank]}"
+        counters[rank] += 1
+        counters[rank] %= num_adapters_per_rank
+        df.at[idx, "adapter_dir"] = adapter
+    
+    df.to_csv(f"./uniform_poisson_{rps}.0_900_50_adapters.csv", index=False)
