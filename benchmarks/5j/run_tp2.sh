e62a04ffaff35ad497d8b45075bec726abadb3e4
Shrikara Arun <t-sarun@microsoft.com>
2025-11-15 05:25:48 +0000

    update run tp scripts 5j

--- benchmarks/5j/run_tp2.sh
+++ benchmarks/5j/run_tp2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-rps_list=(28 32 34)
+rps_list=(32)
 num_servers=4
 # backends=("baseline" "contiguous" "system")
 backends=("system")
@@ -13,16 +13,16 @@ for rps in "${rps_list[@]}"; do
     # trace_file_path="5i/uniform_poisson_${rps}.0_${duration}_30b.csv"
 
     for backend in "${backends[@]}"; do
-        trace_file_path="../server_maps/uniform_poisson_${rps}.0_${duration}_server_maps_${num_servers}_servers/${backend}/uniform_poisson_${rps}.0_${duration}.csv"
+        trace_file_path="../server_maps/uniform_poisson_${rps}.0_${duration}_server_maps_tp2_${num_servers}_servers/${backend}/uniform_poisson_${rps}.0_600.csv"
         mkdir -p ../../../outputs/tp_exp5j/uniform_poisson_${rps}.0_${duration}_tp2_${num_servers}_servers/${backend}
 
         for server_id in $(seq 1 $num_servers); do
-            echo "[$(date -u +'%Y-%m-%dT%H:%M:%SZ')] qps=$rps distribution=uniform arrival=poisson backend=$backend server_idx=$server_id" >> ../../../outputs/run_log_tp_5j.txt
+            echo "[$(date -u +'%Y-%m-%dT%H:%M:%SZ')] tp2 qps=$rps distribution=uniform arrival=poisson backend=$backend server_idx=$server_id" >> ../../../outputs/run_log_tp_5j.txt
 
             python ../run_exp_from_servermaps.py --backend ${backend} --trace-file-path ${trace_file_path} --servers "http://10.0.0.${server_id}:8000" --output uniform_poisson_${rps}.0_${duration}_${num_servers}_servers_server_${server_id}.txt --warmup-time 0 --warmup-requests 0 
             rc=$?
             if [ $rc -ne 0 ]; then
-                echo "[$(date -u +'%Y-%m-%dT%H:%M:%SZ')] FAILED qps=$rps distribution=uniform arrival=poisson backend=$backend server_idx=$server_id exit_code=${rc}" >> ../../../outputs/run_log_tp_5j.txt
+                echo "[$(date -u +'%Y-%m-%dT%H:%M:%SZ')] FAILED tp2 qps=$rps distribution=uniform arrival=poisson backend=$backend server_idx=$server_id exit_code=${rc}" >> ../../../outputs/run_log_tp_5j.txt
             fi
 
             mv uniform_poisson_${rps}.0_${duration}_${num_servers}_servers_server_${server_id}.txt ../../../outputs/tp_exp5j/uniform_poisson_${rps}.0_${duration}_tp2_${num_servers}_servers/${backend}/uniform_poisson_${rps}.0_${duration}_${num_servers}_servers_server_${server_id}.txt
